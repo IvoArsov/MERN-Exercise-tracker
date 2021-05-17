e9eebbe08062c3f85c5c016a8e05c665c8e5cd02
IvoArsov <arsov.ivaylo@gmail.com>
2021-05-17 16:42:32 +0300

    Make part of the front end

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+
+export default class EditExercise extends Component{
+    render(){
+        return(
+            <div>
+                <p>Edit Exercises Component</p>
+            </div>
+        )
+    }
+}
---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+
+export default class ExercisesList extends Component{
+    render(){
+        return(
+            <div>
+                <p>Exercises List Component</p>
+            </div>
+        )
+    }
+}
